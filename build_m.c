8daa4cb073380bd1d6c05b340e3ce4115a91e9a5
Jason Bacon <bacon@coral.acadix.biz>
2020-05-19 16:43:19 -0500

    Upstream patches from pkgsrc

--- build_m.c
+++ build_m.c
@@ -861,7 +861,7 @@ void    set_makefile(proj_t *project, char *makefile, file_t *file,
 void    init_makefile(proj_t *project,char *makefile,file_t *file,opt_t *options)
 
 {
-    strlcpy(project->make_cmd, "make", PATH_MAX);
+    strlcpy(project->make_cmd, "make", CMD_LEN);
     strlcpy(project->makefile, makefile, PATH_MAX);
     
     /*
